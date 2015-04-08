44e2b6002070dc9d80bf8f8a99c923223078dee7
Christopher <chris.jaramillo@gmail.com>
2015-04-08 06:52:01 -0700

    Added turtle class and unit tests

---
+++ UnitTests/TurtleTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Christopher on 4/8/2015.
+ */
+public class TurtleTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMove() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testTurn() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPenUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPenDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testIsPenUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDirection() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLocation() throws Exception {
+
+    }
+}
---
+++ src/Turtle.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+/**
+ * Created by Christopher on 4/8/2015.
+ */
+public class Turtle {
+    public void move(int distance){}
+
+    public void turn(){}
+
+    public void penUp(){}
+
+    public void penDown(){}
+
+    public boolean isPenUp(){}
+
+    public int direction(){}
+
+    public Point location(){}
+}
